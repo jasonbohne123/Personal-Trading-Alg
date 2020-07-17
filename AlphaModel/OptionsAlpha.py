50597669cdf1d2be8a8103d4a4e9e2462b660a8f
Jason Bohne <jbohne822@gmail.com>
2020-07-16 22:35:15 -0500

    Update OptionsAlpha.py

--- AlphaModel/OptionsAlpha.py
+++ AlphaModel/OptionsAlpha.py
@@ -7,12 +7,12 @@ AddReference("QuantConnect.Algorithm.Framework")
 AddReference("QuantConnect.Indicators")
 
 class alpha(AlphaModel):   
-    def __init__(self,period = 75,resolution = Resolution.Daily):
+    def __init__(self,period = 63,resolution = Resolution.Daily):
       
         self.period = period
         self.resolution = resolution
         self.insightPeriod = Time.Multiply(Extensions.ToTimeSpan(resolution), period)
-        self.symbolDataBySymbol ={} # this is just an dictcionary
+        self.symbolDataBySymbol ={} 
         self.optionDataBySymbol ={}
         self.removed=[]
         
@@ -28,19 +28,19 @@ class alpha(AlphaModel):
         if algorithm.Time.day == self.day:
             return []
         #Currently we only want to emit insights once a day
-     
+         
         
         for symbol, symbolData in self.symbolDataBySymbol.items():
             
+            if not data.Bars.ContainsKey(symbol):
+                continue
+            
+            #Pulls our symbol specific indicators
             value = algorithm.Securities[symbol].Price
             std=symbolData.STD
             ema=symbolData.EMA
-            #Initalizes our indicators and contracts
             putcontract=None
             callcontract=None
-            
-            
-  
             
             for contract ,info in self.optionDataBySymbol.items():
           
@@ -52,93 +52,72 @@ class alpha(AlphaModel):
                     callcontract=contract
                   
             
-                
+            #The Alpha strategy longs if equity current price is a standard deviation below EMA and shorts if vice versa
+            #Emits flat otherwise
             if value< (ema.Current.Value-std.Current.Value):
                 insights.append(Insight.Price(symbol, timedelta(days=1), InsightDirection.Up, 0.0025, 1.00,"Options", .5))
                 
                 #If we are longing equity and have not already bought a put contract and one exists buy one
                 if putcontract is not None and not algorithm.Portfolio[putcontract].Invested:
-                    insights.append(Insight.Price(putcontract, timedelta(days=1), InsightDirection.Up, 0.0025, 1.00,"Options", .5))
-                    algorithm.Log(str(putcontract))
-                    
+                    algorithm.MarketOrder(putcontract,1,True)
                    #If we are trying to buy a put and we already have a call on the equity , we should sell it
+                    
                     if callcontract is not None and algorithm.Portfolio[callcontract].Invested:
-                        insights.append(Insight.Price(callcontract, timedelta(days=1), InsightDirection.Flat,0.0025, 1.00,"ReversiontotheMean", .5))
-                        
+                        algorithm.MarketOrder(callcontract,-1,True)
              
                      
             elif value> (ema.Current.Value+std.Current.Value):
-            
                 insights.append(Insight.Price(symbol, timedelta(days=1), InsightDirection.Down,0.0025, 1.00,"Options", .5))
                 
                 #If we are shorting equity and have not already bought a put contract and one exists buy one  
                 if callcontract is not None and not algorithm.Portfolio[callcontract].Invested:
-
-                 
-                    insights.append(Insight.Price(callcontract, timedelta(days=1), InsightDirection.Up, 0.0025, 1.00,"Options", .5))
-                    algorithm.Log(str(callcontract))
-                    
+                    algorithm.MarketOrder(callcontract,1,True)
+                   
                     #If we are trying to buy a call and we already have a put on the equity , we should sell it
                     if  putcontract is not None and algorithm.Portfolio[putcontract].Invested:
-                        insights.append(Insight.Price(putcontract ,timedelta(days=1), InsightDirection.Flat,0.0025, 1.00,"ReversiontotheMean", .5))
-            
+                        algorithm.MarketOrder(putcontract,-1,True)
             else:
                 insights.append(Insight.Price(symbol, timedelta(days=1), InsightDirection.Flat,0.0025, 1.00,"ReversiontotheMean", .5))
-                  
+
+        if insights:
+            self.day = algorithm.Time.day
+
         return insights
-            
+
 
 
     def OnSecuritiesChanged(self, algorithm, changes):
-      
- 
-                    
         for y in  changes.RemovedSecurities:
-     
             #There are two ways which we can remove options
             
             #1 if an equity is no longer in our universe remove it and corresponding equity
             if y.Symbol.SecurityType ==SecurityType.Equity:   
                 self.removed.clear()
                 for contract ,info in self.optionDataBySymbol.items():
-                    
-                    
                    if info.Underlying==y.Symbol.Value:
                         self.removed.append(contract)
                        #pull all options with equity underlying it and add to self.removed
-                   
-               
+                
                 for x in self.removed :
-                    
                     optionData=self.optionDataBySymbol.pop(x,None)
-                   #if optionData:
-                       # algorithm.SubscriptionManager.RemoveConsolidator(x, optionData.consolidator)
-                       #liquidate and remove options all at once
+                      #As we do not create any indicators with options we do not have any consolidators that we need to remove here
                     
                 symbolData = self.symbolDataBySymbol.pop(y.Symbol, None)
                
-               # if symbolData:
-                   # algorithm.SubscriptionManager.RemoveConsolidator(y.Symbol, symbolData.EMAconsol)
-                   # algorithm.SubscriptionManager.RemoveConsolidator(y.Symbol, symbolData.STDconsol)
+                if symbolData:
+                    algorithm.SubscriptionManager.RemoveConsolidator(y.Symbol, symbolData.Consolidator)
+                    #Remove our consolidators to not slow down our algorithm
             
             #If the option is no longer the desired one and we also arent removing the equity remove it
             elif y.Symbol.SecurityType ==SecurityType.Option:
-          
                 if y.Underlying not in [x.Symbol for x in changes.RemovedSecurities]:
-                   
                     optionData=self.optionDataBySymbol.pop(y.Symbol,None)
-                   
-                   # if optionData:
-                       # algorithm.SubscriptionManager.RemoveConsolidator(y.Symbol, optionData.consolidator)#liquidate and remove options all at once
-                    
-      
-                    
-       
-   
+                  #As we do not create any indicators with options we do not have any consolidators that we need to remove here
+
       
         addedSymbols = [ x.Symbol for x in changes.AddedSecurities if (x.Symbol not in self.symbolDataBySymbol and x.Symbol.SecurityType ==SecurityType.Equity)]
      
-        #makes symbol instance for new equities in our universe
+    
         if len(addedSymbols) == 0: return
         #if no new symbols we do not need to generate any new instances
         
@@ -146,11 +125,12 @@ class alpha(AlphaModel):
         #pulls history for all new symbols
  
         for symbol in addedSymbols:
-
-            std=algorithm.STD(symbol, self.period, self.resolution)
-
-            ema=algorithm.EMA(symbol, self.period, self.resolution)
-        
+            
+            ema=ExponentialMovingAverage(self.period)
+            std=StandardDeviation(self.period)
+            consolidator = algorithm.ResolveConsolidator(symbol, self.resolution)
+            algorithm.RegisterIndicator(symbol, std, consolidator)
+            algorithm.RegisterIndicator(symbol, ema, consolidator)
             #for each new symbol, generate an instance of the indicator std and ema
             
             if not history.empty:
@@ -163,15 +143,19 @@ class alpha(AlphaModel):
                     std.Update(tuple.Index, tuple.close)
       
             
-            self.symbolDataBySymbol[symbol] = SymbolData(symbol, ema,std)
-   
+            self.symbolDataBySymbol[symbol] = SymbolData(symbol, ema,std,consolidator)
+            #Records Symbol Data of each symbol including indicators and consolidator
             
         options= [ x.Symbol for x in changes.AddedSecurities if (x.Symbol not in self.optionDataBySymbol and x.Symbol.SecurityType ==SecurityType.Option)]
         if len(options) == 0: return
+    
+        # Assign the option underlying
+        for option in options:
+            algorithm.Securities[option].Underlying = algorithm.Securities[option.Underlying] 
 
-        # pulls options instances
+       
         newhistory = algorithm.History(options, self.period, Resolution.Minute)
-        #Need resolution.Minute for options to show up insights
+    
         if  newhistory.empty: return
         #if no new symbols we do not need to generate any new instances
         
@@ -187,17 +171,16 @@ class alpha(AlphaModel):
                 right="put"
           
             self.optionDataBySymbol[contract] = OptionData(contract,underlying,right)
-       #Records Option Data instance of each contract underlying symbol along with it being a put or call
+       #Records Option Data  of each contract underlying symbol along with it being a put or call
   
 class SymbolData:
-    '''Contains data specific to a symbol required by this model'''
-    def __init__(self, symbol, ema ,std):
+    def __init__(self, symbol, ema ,std,consolidator):
         self.Symbol = symbol
         self.EMA  = ema
         self.STD=std
+        self.Consolidator=consolidator
         
 class OptionData:
-    '''Contains data specific to a symbol required by this model'''
     def __init__(self,contract, underlying,right):
         self.Contract=contract
         self.Underlying=underlying
